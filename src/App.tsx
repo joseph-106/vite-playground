25d2ffcfab86e28943db083d5d18c7e5f414654e
unknown <sjjoseph106@gmail.com>
2022-08-18 20:50:38 +0900

    :technologist: 커스텀 테마

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { vite } from "@/assets/img";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <img src={vite} className="w-64" />
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-pretendard text-primary">Hello World!</h1>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: { primary: "#222222" },
+    fontFamily: {
+      pretendard: ["Pretendard", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
