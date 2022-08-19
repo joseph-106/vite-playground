e92e0eedb4fd95b38f39e3ebed11edf7ca5e9adc
unknown <sjjoseph106@gmail.com>
2022-08-19 19:53:04 +0900

    :coffin: 캐시 삭제

--- src/components/Counter/Count.tsx
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from "recoil";
-import { charCountState } from "@/state/counter";
-
-const Count = () => {
-  const count = useRecoilValue(charCountState);
-  return <div>Count: {count}</div>;
-};
-
-export default Count;
--- src/components/Counter/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as TextInput } from "@/components/counter/TextInput";
-export { default as Count } from "@/components/counter/Count";
